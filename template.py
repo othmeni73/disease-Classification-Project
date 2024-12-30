647d389b4f0d9e028d6502e40e4b9fbd05621279
unknown <othmenizaineb@yahoo.fr>
2024-12-30 15:56:24 +0100

    day2

--- template.py
+++ template.py
@@ -15,6 +15,7 @@ list_of_files = [
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
+
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
@@ -22,18 +23,21 @@ list_of_files = [
     "params.yaml",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb" # reserved for some notebook experiences
+    "research/trials.ipynb", # reserved for some notebook experiences
+    "templates/index.html"
 
     ]
+for filepath in list_of_files :
 
-    for filepath in list_of_files :
-        filepath = Path(filepath)
-        filedir , filename = os.path.split(filepath)
+    filepath = Path(filepath)
+    filedir , filename = os.path.split(filepath)
 
-        if filedir != "":
-            os.makedirs(filedir, exist_ok = True)
-            logging.info(f"creating directory {filedir} for the file {filename}")
-        if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0 ):
-            with open(filepath,"w") as f:
-                pass
-                logging.info(f"")
+    if filedir != "":
+        os.makedirs(filedir, exist_ok = True)
+        logging.info(f"creating directory {filedir} for the file {filename}")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0 ):
+        with open(filepath,"w") as f:
+            pass
+            logging.info(f"creating empty file; {filepath}")
+    else:
+        logging.info(f"filename {filepath} is already created")
